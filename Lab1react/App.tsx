a451eb8503233b0b9ebba8ec5b055a2c588a9ce8
Meyflex <mohamdimagh1@gmail.com>
2023-11-01 20:09:45 +0100

    4 of them

---
+++ Lab1react/App.tsx
@@ -0,0 +1,63 @@
+import { StatusBar } from 'expo-status-bar';
+import { Button, StyleSheet, Text, TextInput, View ,Image} from 'react-native';
+
+export default function App() {
+   const IMAGENAME = require('./assets/image.png'); 
+
+  return (
+    <View style={styles.container}>
+      <Image source={IMAGENAME} style={styles.Image} />
+      <View style={styles.ButtonContainer} >
+        <Button title='Button'/> 
+        <Button title='Button'/> 
+      </View>
+        
+      <View style={styles.ButtonContainer} >
+        <Button title='Button'/> 
+        <Button title='Button'/> 
+      </View>
+      
+      <View style={styles.TextInputContainer} >
+        <Text>Email</Text>
+        <TextInput style={styles.TextInput} />
+      </View>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingTop:60
+  },
+  ButtonContainer:{
+
+    justifyContent :'space-around',
+    alignItems: 'center',
+    flexDirection:'row',
+    width:'100%',
+    margin:15
+  },
+  TextInputContainer:{
+    justifyContent :'space-evenly',
+    alignItems: 'center',
+    flexDirection:'row',
+    width:'100%',
+    margin:15
+  },
+  TextInput :{
+    borderColor:'black',
+    borderBottomWidth:2,
+    width: '60%',
+    backgroundColor:"#EEE"
+  },
+  Image:{
+    height: 200,
+    width:200
+
+  }
+
+});
